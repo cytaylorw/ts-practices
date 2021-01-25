82aa7cd60f9cec677a5516d52d9217b6683cad01
Taylor <cytaylorw@gmail.com>
2021-01-25 22:50:59 +0800

    [reverse-nested-object] Reverse deep object

--- reverse-nested-object/src/index.ts
+++ reverse-nested-object/src/index.ts
@@ -3,6 +3,13 @@ export type DeepObject<T> = {[key: string]: ValueOrObject<T> };
 
 export const reverse = (input: DeepObject<String>): DeepObject<String> => {
     let [key, value] = Object.entries(input)[0];
-    if(typeof value === 'string') return {[value]: key};
-    return input;
+    let output: DeepObject<String> | undefined;
+
+    while(typeof value === 'object'){
+        let [nextKey, nextValue] = Object.entries(value)[0];
+        output = output ? {[nextKey]: output} : {[nextKey]: key};
+        value = nextValue;
+    }
+    output = output ? {[value]: output} : {[value]: key};
+    return output;
 }
