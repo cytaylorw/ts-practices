817a2a38dec90474023b9f8beb34565499a4103f
Taylor <cytaylorw@gmail.com>
2021-01-25 22:30:50 +0800

    [reverse-nested-object] Reverse simple object

--- reverse-nested-object/src/index.ts
+++ reverse-nested-object/src/index.ts
@@ -1,6 +1,8 @@
 export type ValueOrObject<T> = T | {[key: string]: ValueOrObject<T> };
 export type DeepObject<T> = {[key: string]: ValueOrObject<T> };
 
-export const reverse = (input: DeepObject<String>) => {
+export const reverse = (input: DeepObject<String>): DeepObject<String> => {
+    let [key, value] = Object.entries(input)[0];
+    if(typeof value === 'string') return {[value]: key};
     return input;
 }
